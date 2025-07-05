b5382c0af62b135c16c40edb7571bbfef07d9944
gbane abou <abougbane889@gmail.com>
2025-07-05 10:16:45 +0000

    first

---
+++ src/content_option.js
@@ -0,0 +1,142 @@
+const logotext = "GbaneAbou";
+const meta = {
+    title: "Gbane Abou",
+    description: "I’m Gbane Abou _ Full stack devloper,currently working in Abidjan",
+};
+
+const introdata = {
+    title: "Je me nome GBANE ABOU",
+    animated: {
+        first: "J'aime Coder",
+        second: "Je suis développeur Fullstack",
+        third: " et mobile aussi",
+    },
+    description: "Etudiant fraichement diplômer d'un BTS en informatique développeur d'application, ravi d'être a votre service",
+    your_img_url: "/images/Snapchat-266426669.jpg",
+};
+
+const dataabout = {
+    title: "un peu sur moi",
+    aboutme: "Développeur Fullstack à la recherche d'opportunité pour appliquer mes connaissances et gagner en compétences",
+};
+const worktimeline = [{
+        jobtitle: "Développeur Fullstack en chef chez AFRICK CONTRACTOR",
+        where: "rivera bonoumin",
+        date: "Fevrier 2024",
+    },
+    {
+        jobtitle: "Informaticien",
+        where: "Koumassi Saint-Etienne",
+        date: "Mai-maintenant",
+    },
+    {
+        jobtitle: "Freelance developpeur",
+        where: "Koumassi sopim",
+        date: "2024-maintenant",
+    },
+    
+];
+
+const skills = [{
+        name: "JAVA UX/UI",
+        value: 50,
+    },
+    {
+        name: "LARAVEL",
+        value: 70,
+    },
+    {
+        name: "REACT",
+        value: 80,
+    },
+    {
+        name: "PHP",
+        value: 60,
+    },
+    {
+        name: "SQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Le UI Design séduit l’œil, le UX Design simplifie la vie,Les deux sont complémentaires et essentiels pour créer des applications réussies.",
+
+    },
+    {
+        title: "Mobile Apps",
+        description: "",
+    },
+    {
+        title: "Web sites et Apps",
+        description: "",
+    },
+];
+
+const dataportfolio = [{
+        img: "/images/page jbc.png",
+        description: "SITE WEB DE JEUNESSE BASKET CLUB",
+        link: "#",
+    },
+    {
+        img: "/images/img app.png",
+        description: "APP MOBILE POUR EQUIPE DE BASKET-BALL",
+        link: "#",
+    },
+    {
+        img: "/images/img pro.png",
+        description: "ALTERNATIVE WHATAPP",
+        link: "#",
+    },
+    {
+        img: "/images/Capture d’écran 2024-08-01 140957.png",
+        description: "APP WEB DE GESTION IMMOBILIERE",
+        link: "#",
+    },
+    {
+        img: "/images/muscu app.png",
+        description: "APP DE SUIVI SPORTIF PERSONNEL",
+        link: "#",
+    },
+    {
+        img: "/images/ecommerce.png",
+        description: "UX/UI DE APP E-COMMERCE",
+        link: "#",
+    },
+
+    {
+        img: "/images/instaabou.png",
+        description: "RESEAUX SOCIAL INSTAABOU",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abcdefg12345abou@gmail.com",
+    YOUR_FONE: "0151360596 - 0544879036",
+    description: "Contactez moi pour tout projet personnel ou entreprise",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+     
+};
+
+const socialprofils = {
+    github: "https://github.com/Bebeadft",
+    facebook: "https://www.facebook.com/profile.php?id=100092390359478",
+    linkedin: "https://https://www.linkedin.com/in/abou-gbane-94361a28b/",
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
